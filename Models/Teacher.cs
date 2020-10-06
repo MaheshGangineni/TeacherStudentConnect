e86e882a4fceabde63aa9f565e7e52ca95a1d603
MaheshGangineni <69962004+MaheshGangineni@users.noreply.github.com>
2020-10-06 13:51:22 +0530

    Add files via upload

---
+++ Models/Teacher.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TeacherStudentConnect.Models
+{
+    public class Teacher
+    {
+        public String Specialisation { get; set; }
+        public string Remarks { get; set; }
+        public String Proficiency { get; set; }
+    }
+}
